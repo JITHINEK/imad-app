95e18bf6cedc1e3d5d18b06713d8dfa2957809d0
JITHINEK <36569684+JITHINEK@users.noreply.github.com>
2018-02-21 21:14:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,86 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles : {
+    var articleOne = {
+        title:'Article One | JITHIN Ek',
+        heading: 'Article One',
+        date:'Sep 5,2018',
+        content:`<p>
+                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+                    </p>
+                    <p>
+                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+                    </p>
+                    <p>
+                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+                    </p>`
+    },
+    var articleTwo = {
+        title:'Article Two | JITHIN Ek',
+        heading: 'Article Two',
+        date:'Sep 5,2018',
+        content:`<p>
+                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+                    </p>
+                    <p>
+                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+                    </p>
+                    <p>
+                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+                    </p>`
+    },
+    var articleThree = {
+        title:'Article Two | JITHIN Ek',
+        heading: 'Article Two',
+        date:'Sep 5,2018',
+        content:`<p>
+                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+                    </p>
+                    <p>
+                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+                    </p>
+                    <p>
+                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+                    </p>`
+    }
+};
+
+function createTemplate (data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    
+    var htmlTemplate = `<html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta neme='viewport' content="width=device-width, initial=scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class='container'>
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>${heading}</h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>`
+    return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -17,7 +97,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(htmlTemplate(articleOne))
 });
 app.get('/article-two',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
