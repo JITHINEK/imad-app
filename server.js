2946e7861aad406e7c171ae45c714ea919f2373b
JITHINEK <36569684+JITHINEK@users.noreply.github.com>
2018-02-21 20:06:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ app.get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
-    res.send('Article two requested and will be served here.')
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three',function(req,res){
-    res.send('Article three requested and will be served here.')
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
