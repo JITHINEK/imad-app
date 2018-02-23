c4db1fb7f57fc292ee7188270ab98875d9364bca
JITHINEK <36569684+JITHINEK@users.noreply.github.com>
2018-02-23 18:24:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,6 +97,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/:articleName',function(req,res){
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 app.get('/article-two',function(req,res){
