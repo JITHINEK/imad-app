c368d28d1cb17493a703cf55a772b454254b56ea
JITHINEK <36569684+JITHINEK@users.noreply.github.com>
2018-02-25 18:19:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,25 @@
-console.log('Loaded!');
+
 
 var button = document.getElementById('counter');
 
 button.onclick = function(){
+    //create a request obj
     var request = new XMLHttpRequest();
     
+    //capture the request and store it in a variable
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
             if(request.status ===200){
                 var counter = counter.response.Text;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
             }
         }
-        
+        //Not done Yet
     };
-
+    
+    //make a request
     request.open('GET','http://jithinek96.imad.hasura-app.io/counter',true);
     request.sent(null);
     
