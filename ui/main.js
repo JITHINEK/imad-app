b9df04e9b30b28f268b5d8d27a940f83c659643b
JITHINEK <36569684+JITHINEK@users.noreply.github.com>
2018-02-25 17:24:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,6 +16,6 @@ function moveRight(){
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,50);
     
 };
