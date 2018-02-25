b08d5ae648f402408048fb6eb7777a0de5088fb6
JITHINEK <36569684+JITHINEK@users.noreply.github.com>
2018-02-25 19:00:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,6 +22,20 @@ button.onclick = function(){
     //make a request
     request.open('GET','http://jithinek96.imad.hasura-app.io/counter',true);
     request.send(null);
-    
+};
 
+
+//submit name
+
+var nameInput = document.getElementById('name');
+var name = nameIput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    var names = ['name1','name2','name3','name4']
+    var list = '';
+    for(var i=0;i<names.length;i++){
+        list +- 'li' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
 };
