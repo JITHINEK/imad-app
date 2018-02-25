b56c52ba653b43eecbe02c9064bed2c99f7efbec
JITHINEK <36569684+JITHINEK@users.noreply.github.com>
2018-02-25 17:41:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,12 @@ function createTemplate (data){
     return htmlTemplate;
 }
 
+app.get('/counter',function(req,res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+}); 
 
+var counter = 0;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
